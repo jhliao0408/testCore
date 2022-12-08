ea5a701e07d22cc6384e0cee227a69e586421f6e
jhliao0408 <jhliao0408@gmail.com>
2022-12-08 16:01:25 +0800

    testCore

---
+++ test1/test1/Controllers/testController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test1.Controllers
+{
+    public class testController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Msg = "test test test!!";
+            return View();
+        }
+    }
+}
